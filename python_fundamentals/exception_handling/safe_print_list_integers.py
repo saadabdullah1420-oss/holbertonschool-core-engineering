49830b83e4c3af7b490f9ed8b09618db19ecd903
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-09-26 20:06:45 +0300

    Fix safe list integer handling

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -3,18 +3,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    i = 0
 
-    while i < x:
+    for i in range(x):
         try:
-            if "{:d}".format(my_list[i]):
-                print(my_list[i], end="")
-                count += 1
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
         except (ValueError, TypeError):
             pass
-        except IndexError:
-            break
-        i += 1
 
     print()
     return count
