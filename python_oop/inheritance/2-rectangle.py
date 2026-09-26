ee6841338d7e18da76283e84f1e632a4df81f928
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-09-26 20:34:34 +0300

    Add full Rectangle class

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module for the Rectangle class."""
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the rectangle area."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the rectangle description."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
