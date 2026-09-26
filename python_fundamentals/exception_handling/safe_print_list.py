d50a06dde943d800669c4e680dfc6c06cd445f57
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-09-26 19:52:09 +0300

    Add safe list printing

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-"""Module for safe_print_list function."""
 
 
 def safe_print_list(my_list=[], x=0):
-    """Print x elements of my_list, catching IndexError.
-
-    Returns the real number of elements printed.
-    """
     count = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end='')
+
+    while count < x:
+        try:
+            print(my_list[count], end="")
             count += 1
-    except IndexError:
-        pass
+        except IndexError:
+            break
+
     print()
     return count
